3d27bbdb0ac6346fd640e818b5552fa998cd044c
owenaraujo <owendjesus64@gmail.com>
2021-06-06 18:50:40 -0400

    agregar patj static files

--- src/public/js/chunk-2d20829d.a5efc972.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d20829d"],{a42f:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" favoritos ")])},a=[],c=t("2877"),l={},o=Object(c["a"])(l,u,a,!1,null,null,null);e["default"]=o.exports}}]);
-//# sourceMappingURL=chunk-2d20829d.a5efc972.js.map
--- src/public/js/chunk-2d21f801.87eac4a6.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21f801"],{d9a7:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" pedidos ")])},c=[],l=t("2877"),s={},a=Object(l["a"])(s,u,c,!1,null,null,null);e["default"]=a.exports}}]);
-//# sourceMappingURL=chunk-2d21f801.87eac4a6.js.map
